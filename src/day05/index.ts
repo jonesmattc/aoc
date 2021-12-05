4da386321b2c6c2fed2f380ed1d42613d773facd
Matthew Jones <jones.matt.c@gmail.com>
2021-12-05 00:10:54 -0800

    AOC 2021 Day 5 quick maffs

--- src/day05/index.ts
+++ src/day05/index.ts
@@ -2,15 +2,11 @@ import run from "aocrunner"
 
 const parseInput = (rawInput: string) => rawInput.split("\n").map(a=>a.split(' -> ').map(a=>a.split(',').map(Number).reverse()));
 
-
-const part1 = (rawInput: string) => {
-    const input: number[][][] = parseInput(rawInput);
+function getEmptyResultMap(input: number[][][]) {
     const xs = input.flatMap(i=>[i[0],i[1]]).map(i=>i[0]);
     const ys = input.flatMap(i=>[i[0],i[1]]).map(i=>i[1]);
     const maxx = xs.reduce((l,r) =>Math.max(l, r))+1;
     const maxy = ys.reduce((l, r) => Math.max(l, r))+1;
-    let answer = 0;
-
     const result: number[][] = [];
     for(let i =0; i < maxx; i++) {
         let row: number[] = [];
@@ -20,92 +16,50 @@ const part1 = (rawInput: string) => {
         }
     }
 
-    for(const line of input) {
-        const [x1, y1] = line[0];
-        const [x2, y2] = line[1];
-        if (x1 === x2) {
-            const [ymin, ymax] = [y1,y2].sort((a,b)=>a-b);
-            for(let y = ymin; y <= ymax; y++) {
-                if (result[x1][y] == 1) {
-                    answer++;
-                }
-                result[x1][y]++;
+    return result;
+}
 
+const part1 = (rawInput: string) => {
+    const input: number[][][] = parseInput(rawInput);
+    const result = getEmptyResultMap(input);
+    let answer = 0;
+
+    for(const line of input) {
+        const [[x1, y1], [x2, y2]] = line;
+        const xmult = -(x1-x2)/Math.abs(x1-x2) || 0;
+        const ymult = -(y1-y2)/Math.abs(y1-y2) || 0;
+        if (xmult != 0 && ymult != 0) {
+            // diagonal
+            continue;
+        }
+        const steps = Math.max(Math.abs(x1-x2), Math.abs(y1-y2));
+        for(let i = 0; i <= steps; i++) {
+            if (result[x1+xmult*i][y1+ymult*i] === 1) {
+                answer++;
             }
-        } else if (y1 === y2) {
-            const [xmin, xmax] = [x1,x2].sort((a,b)=>a-b);
-            for (let x = xmin; x <= xmax; x++) {
-                if (result[x][y1] == 1) {
-                    answer++;
-                }
-                result[x][y1]++;
-            }
+            result[x1+xmult*i][y1+ymult*i]++;
         }
     }
-
-
     return answer;
 }
 
 const part2 = (rawInput: string) => {
     const input: number[][][] = parseInput(rawInput);
-    const xs = input.flatMap(i=>[i[0],i[1]]).map(i=>i[0]);
-    const ys = input.flatMap(i=>[i[0],i[1]]).map(i=>i[1]);
-    const maxx = xs.reduce((l,r) =>Math.max(l, r))+1;
-    const maxy = ys.reduce((l, r) => Math.max(l, r))+1;
+    const result = getEmptyResultMap(input);
     let answer = 0;
 
-    const result: number[][] = [];
-    for(let i =0; i < maxx; i++) {
-        let row: number[] = [];
-        result.push(row);
-        for(let j = 0; j < maxy; j++) {
-            row.push(0);
-        }
-    }
-
     for(const line of input) {
-        const [x1, y1] = line[0];
-        const [x2, y2] = line[1];
-        if (x1 === x2) {
-            const [ymin, ymax] = [y1,y2].sort((a,b)=>a-b);
-            for(let y = ymin; y <= ymax; y++) {
-                if (result[x1][y] == 1) {
-                    answer++;
-                }
-                result[x1][y]++;
-
+        const [[x1, y1], [x2, y2]] = line;
+        const xmult = -(x1-x2)/Math.abs(x1-x2) || 0;
+        const ymult = -(y1-y2)/Math.abs(y1-y2) || 0;
+        const steps = Math.max(Math.abs(x1-x2), Math.abs(y1-y2));
+        for(let i = 0; i <= steps; i++) {
+            if (result[x1+xmult*i][y1+ymult*i] === 1) {
+                answer++;
             }
-        } else if (y1 === y2) {
-            const [xmin, xmax] = [x1,x2].sort((a,b)=>a-b);
-            for (let x = xmin; x <= xmax; x++) {
-                if (result[x][y1] == 1) {
-                    answer++;
-                }
-                result[x][y1]++;
-            }
-        } else {
-            let xmult = 1;
-            let ymult = 1;
-            if (x1 > x2) {
-                xmult = -1;
-            }
-            if(y1 > y2) {
-                ymult = -1;
-            }
-            for(let i = 0; i <= Math.abs(x1-x2); i++) {
-                if (result[x1+xmult*i][y1+ymult*i] === 1) {
-                    answer++;
-                }
-                result[x1+xmult*i][y1+ymult*i]++;
-            }
+            result[x1+xmult*i][y1+ymult*i]++;
         }
-        // console.log(x1,x2,y1,y2);
-        // console.log(result.map(a=>a.map(b=>b===0?'.':b.toString()).join("")).join("\n"));
-        // console.log("\n");
     }
-
-
     return answer;
 }
 
